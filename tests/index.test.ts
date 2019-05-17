e68291fee1b2ce4a21440b49e82e181bd9602a74
luying3 <myluluy@gmail.com>
2019-05-17 14:33:39 +0800

    add test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,21 +1,128 @@
-import { expect } from 'chai';
-import myClass from '../src/index';
+// tslint:disable-next-line:no-var-requires
+const Koa = require('koa');
+// tslint:disable-next-line:no-var-requires
+const request = require('supertest');
+// tslint:disable-next-line:no-var-requires
+const monitor = require('../dist/index').default;
 
-describe('test for myClass', () => {
-  it('test for get method', (done) => {
-    let foo = new myClass({
-      a: 1
-    });
-    expect(foo.get('a')).to.equal(1);
-    done();
+const port = 3000;
+const code200 = 200;
+const code404 = 404;
+const app = new Koa();
+const auth = {
+  name: 'monitor',
+  password: 'monitor'
+};
+
+describe('daruk-monitor-middleware error', function cb() {
+  let server;
+  before((done) => {
+    app.use(
+      monitor({
+        auth
+      })
+    );
+
+    server = app.listen(port, done);
   });
-  it('test for get method', (done) => {
-    let bar = new myClass({
-      a: 1
-    });
-    expect(bar.get('a')).to.equal(1);
-    expect(bar.set('a', 2)).to.equal(2);
-    expect(bar.get('a')).to.equal(2);
-    done();
+
+  after((done) => {
+    server.close(done);
+  });
+
+  it('/monitor/ start up with v8ProfilerPath error', (done) => {
+    request(server)
+      .get('/monitor/profiler/function')
+      .expect(code200)
+      .auth(auth.name, auth.password)
+      .expect((res) => {
+        return res.text === `The argument 'id' must be a non-empty string. Received ''`;
+      })
+      .end(done);
+  });
+});
+
+describe('daruk-monitor', function cb() {
+  let server;
+  before((done) => {
+    app.use(
+      monitor({
+        auth
+      })
+    );
+
+    server = app.listen(port, done);
+  });
+
+  after((done) => {
+    server.close(done);
+  });
+
+  it('/monitor auth failed', function(done) {
+    // tslint:disable-next-line
+    this.timeout(1000);
+
+    request(server)
+      .get('/monitor/profiler?period=1000')
+      .expect(code200)
+      .auth(auth.name, auth.password + 'error')
+      .end((err, res) => {
+        if (err.message === 'expected 200 "OK", got 401 "Unauthorized"') {
+          done();
+        }
+      });
+  });
+
+  it('/monitor/not-found should not throw error when request non-existing route', (done) => {
+    request(server)
+      .get('/monitor/not-found')
+      .auth(auth.name, auth.password)
+      .expect(code404)
+      .end(done);
+  });
+
+  it('/monitor/profiler', (done) => {
+    request(server)
+      .get('/monitor/profiler')
+      .expect(code200)
+      .auth(auth.name, auth.password)
+      .expect((res) => {
+        return 'cpu' in res.body && 'memory' in res.body;
+      })
+      .end(done);
+  });
+
+  it('/monitor/profiler/function', (done) => {
+    request(server)
+      .get('/monitor/profiler/function')
+      .expect(code200)
+      .auth(auth.name, auth.password)
+      .expect((res) => {
+        return res.text;
+      })
+      .end(done);
+  });
+
+  it('/monitor/profiler/mem', function(done) {
+    // tslint:disable-next-line
+    this.timeout(10000);
+    request(server)
+      .get('/monitor/profiler/mem')
+      .expect(code200)
+      .auth(auth.name, auth.password)
+      .end(done);
+  });
+
+  it('/monitor/profiler/mem-analytics', function(done) {
+    // tslint:disable-next-line
+    this.timeout(15000);
+    request(server)
+      .get('/monitor/profiler/mem-analytics')
+      .expect(code200)
+      .auth(auth.name, auth.password)
+      .expect((res) => {
+        return res.body;
+      })
+      .end(done);
   });
 });
